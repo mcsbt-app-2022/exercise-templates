6fe2f73a4f44da15013dde9348b7696ddf5b6fe0
Pepe García <pepe@pepegar.com>
2023-02-08 10:15:06 +0100

    code from class

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import csv
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "some secret string"
 
 # args and kwargs in python
 
 @app.route("/")
 def index():
-    num = 0
-    list_of_users = []
-    with open("data/users.csv") as file:
-        reader = csv.reader(file)
-        for line in reader:
-            list_of_users.append(line)
-            num = num + 1
+    if "username" in session:
+        num = 0
+        list_of_users = []
+        with open("data/users.csv") as file:
+            reader = csv.reader(file)
+            for line in reader:
+                list_of_users.append(line)
+                num = num + 1
 
-    return render_template("index.html",
-        number_of_users=num,
-        users=list_of_users)
+        return render_template("index.html",
+            number_of_users=num,
+            users=list_of_users)
+    else:
+        return redirect("/login")
 
 
 @app.route("/users/<user_id>")
@@ -34,7 +38,16 @@ def user_info(user_id):
         
 @app.route("/login")
 def login_form():
+    if "username" in session:
+        return redirect("/")
     return render_template("login.html")
+
+
+auth_db = {
+    "pepe": "ilovepython",
+    "diego": "slothsarecool",
+}
+
 
 
 @app.route("/handle-login", methods=["POST"])
@@ -42,7 +55,14 @@ def handle_login():
     username = request.form["username"]
     password = request.form["password"]
 
-    return "OK"
+    if username in auth_db and auth_db[username] == password:
+        session["username"] = username
+        return redirect("/")
+    else:
+        return redirect("/login")
+
+
+
 
 @app.route("/about")
 def about():
